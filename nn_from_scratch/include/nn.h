c58e9584fbf1cdff5d45782c3d91962661edde48
Benjamin <benc00@vt.edu>
2025-07-10 19:49:31 -0400

    working sgd

--- nn_from_scratch/include/nn.h
+++ nn_from_scratch/include/nn.h
@@ -5,64 +5,101 @@
 #include <cassert>
 #include <cmath>
 #include <ctime>
+#include <memory>
 #include <vector>
 
 namespace F {
 
-typedef double (*activation_func)(double);
+class Functional {
+  public:
+    virtual double forward(double x) noexcept = 0;
+    virtual double derivative(double x) noexcept = 0;
+    std::vector<double> forward(std::vector<double> x) noexcept;
+    std::vector<double> derivative(std::vector<double> x) noexcept;
+};
 
-double identity(double x) noexcept;
-std::vector<double> identity(std::vector<double> x) noexcept;
-double identity_derivative(double x) noexcept;
-std::vector<double> identity_derivative(std::vector<double> x) noexcept;
+class Identity : public Functional {
+    double forward(double x) noexcept;
+    double derivative(double x) noexcept;
+};
 
-double ReLU(double x) noexcept;
-std::vector<double> ReLU(std::vector<double> x) noexcept;
-double ReLU_derivative(double x) noexcept;
-std::vector<double> ReLU_derivative(std::vector<double> x) noexcept;
+class Sigmoid : public Functional {
+    double forward(double x) noexcept;
+    double derivative(double x) noexcept;
+};
 
-double sigmoid(double x) noexcept;
-std::vector<double> sigmoid(std::vector<double> x) noexcept;
-double sigmoid_derivative(double x) noexcept;
-std::vector<double> sigmoid_derivative(std::vector<double> x) noexcept;
+class ReLU : public Functional {
+    double forward(double x) noexcept;
+    double derivative(double x) noexcept;
+};
 
-double tanh(double x) noexcept;
-std::vector<double> tanh(std::vector<double> x) noexcept;
-double tanh_derivative(double x) noexcept;
-std::vector<double> tanh_derivative(std::vector<double> x) noexcept;
+class Tanh : public Functional {
+    double forward(double x) noexcept;
+    double derivative(double x) noexcept;
+};
 
 double random_weight() noexcept;
 double random_weight(const double min, const double max) noexcept;
+
+std::vector<double> vec_dot(std::vector<double> a,
+                            std::vector<double> b) noexcept;
+std::vector<double> vec_add(std::vector<double> a,
+                            std::vector<double> b) noexcept;
+std::vector<double> vec_sub(std::vector<double> a,
+                            std::vector<double> b) noexcept;
+std::vector<double> scalar_dot(double s, std::vector<double> b) noexcept;
+std::vector<double> scalar_div(double s, std::vector<double> b) noexcept;
+std::vector<double> scalar_add(double s, std::vector<double> b) noexcept;
+std::vector<double> scalar_sub(double s, std::vector<double> b) noexcept;
+std::vector<double> scalar_sub(std::vector<double> b, double s) noexcept;
+
 
 } // namespace F
 
 namespace nn {
 
+
 class LinearLayer {
-private:
-  const size_t input_size;
-  const size_t output_size;
-  F::activation_func f;
-  std::vector<std::vector<double>> weight;
-  std::vector<double> bias;
+  private:
+    const size_t input_size;
+    const size_t output_size;
+    std::unique_ptr<F::Functional> f;
+    std::vector<std::vector<double>> weight;
+    std::vector<double> bias;
+    std::vector<double> input;
+    std::vector<double> output;
+    std::vector<double> deltas;
 
-  void initialize_params();
+  public:
+    LinearLayer(LinearLayer&& other) noexcept = default;
+    LinearLayer& operator=(LinearLayer&& other) noexcept = delete;
+  
+    // LinearLayer is not copyable (implicitly deleted due to unique_ptr member)
+    LinearLayer(const LinearLayer& other) = delete;
+    LinearLayer& operator=(const LinearLayer& other) = delete;
 
-public:
-  LinearLayer(size_t input_size, size_t output_size);
-  LinearLayer(size_t input_size, size_t output_size, F::activation_func f);
-  ~LinearLayer();
-  std::vector<double> forward(const std::vector<double> &input);
-  std::vector<double> backward(const std::vector<double> &output,
-                               const std::vector<double> &target,
-                               const double learning_rate);
+    void initialize_params();
+    void compute_deltas(const std::vector<double>& target_deltas,
+                        const std::vector<std::vector<double>>& next_weights);
+    void update_weights(double learning_rate);
+
+    LinearLayer(size_t input_size, size_t output_size);
+    LinearLayer(size_t input_size, size_t output_size,
+                std::unique_ptr<F::Functional> f);
+    std::vector<double> forward(const std::vector<double>& input);
+    friend class MLP;
 };
 class MLP {
-private:
-public:
-  MLP(size_t n_inputs, size_t n_outputs);
-  ~MLP();
-  std::vector<double> forward(std::vector<double> input);
-  std::vector<double> backward(std::vector<double> input);
+  private:
+    std::vector<LinearLayer> layers; // with activation
+
+  public:
+    MLP(const std::vector<size_t>& sizes);
+    MLP(const std::vector<size_t>& sizes,
+        std::vector<std::unique_ptr<F::Functional>>& functionals);
+    std::vector<double> forward(const std::vector<double>& input);
+    double update(const std::vector<double> &input,
+                               const std::vector<double> &target,
+                               const double learning_rate);
 };
 } // namespace nn
--- nn_from_scratch/src/nn/nn_classes.cpp
+++ nn_from_scratch/src/nn/nn_classes.cpp
@@ -2,160 +2,107 @@
 
 namespace nn {
 
-LinearLayer::LinearLayer(size_t input_size, size_t output_size)
-    : input_size(input_size), output_size(output_size),
-      _F(std::make_unique<F::Identity>()) {
-  initialize_params();
-}
-
 LinearLayer::LinearLayer(size_t input_size, size_t output_size,
-                         std::unique_ptr<F::Functional> &fu)
+                         std::unique_ptr<F::Functional> fu)
+    : LinearLayer::LinearLayer(input_size, output_size) {
+    f = std::move(fu);
+}
+
+LinearLayer::LinearLayer(size_t input_size, size_t output_size)
     : input_size(input_size), output_size(output_size) {
-  _F = std::move(fu);
-  initialize_params();
-}
-
-LinearLayer::~LinearLayer() {}
-
-void LinearLayer::initialize_params() {
-  weight = std::vector<std::vector<double>>(input_size,
-                                            std::vector<double>(output_size));
-  bias = std::vector<double>(output_size);
-  for (size_t i = 0; i < output_size; i++) {
-    bias[i] = F::random_weight();
-    for (size_t j = 0; j < input_size; j++) {
-      weight[j][i] = F::random_weight();
+    weight.resize(output_size, std::vector<double>(input_size));
+    bias.resize(output_size);
+    output.resize(output_size);
+    deltas.resize(output_size);
+    input.resize(input_size);
+    for (size_t i = 0; i < output_size; i++) {
+        bias[i] = F::random_weight();
+        for (size_t j = 0; j < input_size; j++) {
+            weight[i][j] = F::random_weight();
+        }
     }
-  }
-  deltas = std::vector<double>(output_size, 0.0);
+    f = std::make_unique<F::Identity>();
 }
 
-// naive implementation for now
-std::vector<double> LinearLayer::forward(const std::vector<double> &input) {
-  assert(input.size() == input_size);
-  std::vector<double> result(output_size);
-  for (size_t j = 0; j < output_size; j++) {
-    double sum = bias[j];
-    for (size_t i = 0; i < input_size; i++) {
-      sum += input[i] * weight[i][j];
+std::vector<double> LinearLayer::forward(const std::vector<double>& x) {
+    input = x;
+    for (size_t i = 0; i < output_size; i++) {
+        double sum = bias[i];
+        for (size_t j = 0; j < input_size; j++) {
+            sum += weight[i][j] * input[j];
+        }
+        output[i] = f->forward(sum);
     }
-    result[j] = _F->forward(sum);
-  }
-  return result;
+    return output;
 }
 
-MLP::MLP(std::vector<size_t> structure,
-         std::vector<std::unique_ptr<F::Functional>> &activations) {
-  size_t sz = structure.size();
-  assert(sz == activations.size() + 1);
-  // layers.reserve(sz - 1);
-  for (size_t i = 0; i < sz - 1; i++) {
-    layers.emplace_back(std::make_unique<LinearLayer>(
-        structure[i], structure[i + 1], activations[i]));
-  }
-}
-
-MLP::~MLP() {}
-
-std::vector<double> MLP::forward(const std::vector<double> &input) {
-  std::vector<double> output = input;
-  for (size_t i = 0; i < layers.size(); i++) {
-    output = layers[i]->forward(output);
-  }
-  return output;
-}
-
-/*
-returns a vector of column vectors organized as follows:
-[ a1 b1 a2 b2 ... ]
- where a represents *before* activation is applied and b is after
- for each layer in the network
-*/
-std::vector<std::vector<double>> MLP::forward(const std::vector<double> &input,
-                                              std::vector<double> &output) {
-  std::vector<std::vector<double>> v(layers.size() * 2 + 1);
-  output = input;
-  v[0] = std::vector<double>(input.size());
-  std::copy(input.begin(), input.end(), v[0].begin());
-  for (size_t i = 0; i < layers.size(); i++) {
-    auto output_size = layers[i]->output_size;
-    auto input_size = layers[i]->input_size;
-    std::vector<double> result(output_size);
-
-    v[2 * i + 1] = std::vector<double>(output_size);
-    v[2 * i + 2] = std::vector<double>(output_size);
-
-    for (size_t j = 0; j < output_size; j++) {
-      double s = layers[i]->bias[j];
-      for (size_t k = 0; k < input_size; k++) {
-        s += output[i] * layers[i]->weight[i][j];
-      }
-      v[2 * i + 1][j] = s;
-      v[2 * i + 2][j] = layers[i]->_F->forward(s);
+void LinearLayer::compute_deltas(
+    const std::vector<double>& target_or_next_deltas,
+    const std::vector<std::vector<double>>& next_weights = {}) {
+    if (next_weights.empty()) { // output layer
+        for (size_t i = 0; i < output_size; ++i) {
+            deltas[i] = (output[i] - target_or_next_deltas[i]) *
+                        f->derivative(output[i]);
+        }
+    } else { // hidden layer
+        for (size_t i = 0; i < output_size; ++i) {
+            double error = 0.0;
+            for (size_t j = 0; j < target_or_next_deltas.size(); ++j) {
+                error += target_or_next_deltas[j] * next_weights[j][i];
+            }
+            deltas[i] = error * f->derivative(output[i]);
+        }
     }
-    output = v[2 * i + 1];
-  }
-  return v;
 }
 
-/*
-updates the parameters of linear layers within the network
+void LinearLayer::update_weights(double learning_rate) {
+    for (size_t i = 0; i < output_size; ++i) {
+        for (size_t j = 0; j < input_size; ++j) {
+            weight[i][j] -= learning_rate * deltas[i] * input[j];
+        }
+        bias[i] -= learning_rate * deltas[i];
+    }
+}
 
-note for a single weight u_ij, the partial derivative of the
-loss function w.r.t. u_ij is:
+MLP::MLP(const std::vector<size_t>& sizes) {
+    for (size_t i = 1; i < sizes.size(); i++) {
+        layers.emplace_back(sizes[i - 1], sizes[i],
+                            std::make_unique<F::Identity>());
+    }
+}
 
-dL       dL    dyj    daj^2
---    =  --  * ---  * -----
-du_ij    dyj  daj^2   du_ij
-
-where i corrspon
-
-*/
-void MLP::backward(const std::vector<double> &input,
-                   const std::vector<double> &target,
+MLP::MLP(const std::vector<size_t>& sizes,
+         std::vector<std::unique_ptr<F::Functional>>& functionals) {
+    assert(sizes.size() == functionals.size() + 1);
+    for (size_t i = 1; i < sizes.size(); i++) {
+        layers.emplace_back(sizes[i - 1], sizes[i],
+                            std::move(functionals[i - 1]));
+    }
+}
+std::vector<double> MLP::forward(const std::vector<double>& x) {
+    std::vector<double> out = x;
+    for (auto& layer : layers) {
+        out = layer.forward(out);
+    }
+    return out;
+}
+double MLP::update(const std::vector<double>& input,
+                   const std::vector<double>& target,
                    const double learning_rate) {
-  std::vector<double> output;
-  auto all_outputs =
-      forward(input, output); // 2 * n_layers + 1 vector of vectors
-  auto dZ = F::scalar_dot(2.0, F::vec_sub(output, target));
-  int n_layers = (int)layers.size();
-  // backprop hidden layers
-  _compute_deltas(layers[n_layers - 1], layers[n_layers - 1], output, target,
-                  true);
-  for (int l = (int)n_layers - 2; l >= 0; l--) {
-    _compute_deltas(layers[l], layers[l + 1], all_outputs[2 * l + 2],
-                    layers[l + 1]->deltas, false);
-  }
-  for (int l = 0; l < n_layers; l++) {
-    auto &layer = layers[l];
-    auto &_input = all_outputs[2 * l];
-    for (size_t i = 0; i < layer->output_size; i++) {
-      for (size_t j = 0; j < layer->input_size; j++) {
-        layer->weight[i][j] -= learning_rate * layer->deltas[i] * _input[j];
-      }
-      layer->bias[i] -= learning_rate * layer->deltas[i];
+    double total_loss = 0.0;
+    auto out = forward(input);
+    for (size_t j = 0; j < out.size(); j++) {
+        total_loss += 0.5 * std::pow(out[j] - target[j], 2);
     }
-  }
-}
-
-void MLP::_compute_deltas(std::unique_ptr<LinearLayer> &l1,
-                          std::unique_ptr<LinearLayer> &l2,
-                          const std::vector<double> &outputs,
-                          const std::vector<double> &target, bool isFinal) {
-  size_t output_size = l1->output_size;
-  if (isFinal) {
-    for (size_t i = 0; i < output_size; i++) {
-      l1->deltas[i] = (outputs[i] - target[i]) * l1->_F->derivative(outputs[i]);
+    // backprop
+    layers.back().compute_deltas(target);
+    for (int l = (int)layers.size() - 2; l >= 0; l--) {
+        layers[l].compute_deltas(layers[l + 1].deltas, layers[l + 1].weight);
     }
-  } else {
-    for (size_t i = 0; i < output_size; i++) {
-      double error = 0.0;
-      for (size_t j = 0; j < target.size(); j++) {
-        error += target[j] * l2->weight[j][i];
-      }
-      l1->deltas[i] = error * l1->_F->derivative(outputs[i]);
+    for (auto& layer : layers) {
+        layer.update_weights(learning_rate);
     }
-  }
+    return total_loss;
 }
 
 } // namespace nn
